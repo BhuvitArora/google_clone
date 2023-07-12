9cbac30747aed2b04b124ec035694e08c08453f7
BHUVIT <bhuvit5506@gmail.com>
2023-07-12 21:12:27 +0530

    first commit

---
+++ my-app/next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    experimental: {
+      appDir: true,
+    },
+    images: {
+      domains: ["upload.wikimedia.org"],
+    },
+  };
+  
+  module.exports = nextConfig;
+  
